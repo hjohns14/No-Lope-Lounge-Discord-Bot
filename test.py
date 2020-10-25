ba2a51471e76b94203997dc3839cf23c6c0c4b8f
hjohns14 <39865626+hjohns14@users.noreply.github.com>
2020-10-25 16:42:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import datetime
 
-## !! TDO Use pytz to localize timestamps// Its actually already on time from the website so good!
+##!!-- Testing new features before adding to bot --!!##
 
-ts = datetime.datetime.fromtimestamp(1575158400000/1000)
+
+
+
+'''ts = datetime.datetime.fromtimestamp(1575158400000/1000)
 military = datetime.time(12,00,00)
 print(military)
 print(ts.time().hour-military.hour, ts.date(), ts.tzinfo)
 print(ts.time().hour - 12)
 time = str(ts.time().hour)
+
+#Maybe 
 if time < 12:
     print(time, "am")
 elif time == 12:
     print(time, "pm")
 else: 
-    print(str(time-12) + "pm")
+    print(str(time-12) + "pm")'''
+
